99791d7f3d61a60192d2d3c5264c9a0c2c3ed2bf
Brandon Newell <newellb@users.noreply.github.com>
2023-04-04 13:25:11 -0400

    Update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-	public static String message = "Greetings from Azure + Tanzu!";
+	public static String message = "Greetings from Brandon + Tanzu!";
 
 	@RequestMapping("/")
 	public String index() {
